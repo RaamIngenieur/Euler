d02f7fa9f9e115e287cf09952555c0746e017a30
Raam <shriraam.mohan@gmail.com>
2017-07-09 20:50:07 +0200

    Sum of even fibonacci

--- Euler/Euler.cpp
+++ Euler/Euler.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 
 int main()
 {
+	int n = 4000000,f = 1, f1= 1;
+	long int sum = 0;
+
+	while (f <= n)
+	{
+		f = f + f1;
+		f1 = f - f1;
+
+		(f % 2 == 0) ? sum += (long int)f : f;
+
+	}
+
+	std::cout << sum << std::endl;
+	std::cin.get();
+
     return 0;
 }
 
