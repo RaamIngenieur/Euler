15a184dbd97efc8498fa9e2e620ce8d978975d75
Raam <shriraam.mohan@gmail.com>
2017-07-10 17:01:34 +0200

    Highest prime factor

--- Euler/Euler.cpp
+++ Euler/Euler.cpp
@@ -7,19 +7,44 @@
 
 int main()
 {
-	int n = 4000000,f = 1, f1= 1;
-	long int sum = 0;
+	unsigned long long n[100000];
+	unsigned long long lim = 600851475143,p;
+	unsigned long long Num = 600851475143;
+	bool isprime;
 
-	while (f <= n)
+	n[0] = 2;
+	p = 0;
+
+	for (unsigned long long t = 3; t <= lim; t++)
 	{
-		f = f + f1;
-		f1 = f - f1;
+		isprime = true;
+		for (int j = 0; j <= p; j++)
+		{
+			if (t%n[j] == 0)
+			{
+				isprime = false;
+				break;
+			}
+		}
 
-		(f % 2 == 0) ? sum += (long int)f : f;
+		if (isprime)
+		{
+			n[p + 1] = t;
+			p++;
 
+			if (Num % t == 0)
+			{
+				Num = Num / t;
+			}
+			else
+			{
+				lim = Num / t;
+			}
+		}
 	}
 
-	std::cout << sum << std::endl;
+	std::cout << std::endl<< p << std::endl;
+	std::cout << std::endl << "Highest prime factor is "<< Num << std::endl;
 	std::cin.get();
 
     return 0;
